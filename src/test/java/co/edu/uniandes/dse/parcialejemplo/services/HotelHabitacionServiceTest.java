d10a16e4a1cc7d04f3400c08fd63b69d101c89d7
Pablo <jp.junco@uniandes.edu.co>
2022-09-23 07:59:14 -0500

    last

---
+++ src/test/java/co/edu/uniandes/dse/parcialejemplo/services/HotelHabitacionServiceTest.java
@@ -0,0 +1,19 @@
+package co.edu.uniandes.dse.parcialejemplo.services;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.transaction.Transactional;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@Transactional
+public class HotelHabitacionServiceTest {
+
+    /**
+     * no alcance a hacer estas pruebas :( pero es el mismo conceptoo.
+     * mercy me :(
+     */
+}
